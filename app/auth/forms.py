d74337c219ec86b96d2215dfdaa79ff3dae4a982
dancan-sandys <dancan.oruko96@gmail.com>
2020-09-21 18:34:22 +0300

    add a sign in and sign up form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import Required, Email, EqualTo
+from ..models import User
+from wtforms import ValidationError
 
 class SignUpform(FlaskForm):
     username = StringField('Preferd username ', validators=[Required()])
@@ -8,9 +10,22 @@ class SignUpform(FlaskForm):
     password = PasswordField('Password', validators=[Required(),EqualTo('confirm_password', message='Your passwords do not match')])
     confirm_password = PasswordField('Confirm Password', validators=[Required()])
 
+    def validate_email(self,data_field):
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('There is an account with that email address')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('The username is already taken')
+
+
+
+
+
 class SignInForm(FlaskForm):
     username = StringField('Username ', validators=[Required()])
     email = StringField('Email', validators=[Required(), Email()])
     password = PasswordField('Password', validators=[Required()])
-
+    remember  =BooleanField('Remember me')
+    submit = SubmitField('Sign in')
 
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,34 @@
 from flask import render_template, render_template,request, url_for
+from . import auth
+from .forms import SignUpform, SignInForm
+from flask_login import login_user
 
-class login():
-    pass
+
+@auth.route('/signup', methods= ["GET","POST"])
+def signup():
+
+    form = SignUpform
+
+    if form.validate_on_submit():
+
+        user = User(user_email =form.email.data, user_username = form.username.data, user_password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.signin'))
+
+        title = 'New Account'
+
+    return render_template('signup.html')
 
 
+@auth.route('/signin')
+def signin():
+    form = SignInForm
+
+    if form.validate_on_submit():
+
+        user = User.query.filter_by(email = form.email.data).first()
+
+        if user is not None:
+            login_user(user, login_form,remember.data)
