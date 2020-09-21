70dd4b08d5f978ddc262073e3b5045947cfdd10a
dancan-sandys <dancan.oruko96@gmail.com>
2020-09-21 18:57:50 +0300

    add a login and a sign up template

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,13 +9,15 @@ class SignUpform(FlaskForm):
     email = StringField('Email', validators=[Required(), Email()])
     password = PasswordField('Password', validators=[Required(),EqualTo('confirm_password', message='Your passwords do not match')])
     confirm_password = PasswordField('Confirm Password', validators=[Required()])
+    submit = SubmitField('Sign Up')
+
 
     def validate_email(self,data_field):
-        if User.query.filter_by(email =data_field.data).first():
+        if User.query.filter_by(user_email =data_field.data).first():
             raise ValidationError('There is an account with that email address')
 
     def validate_username(self,data_field):
-        if User.query.filter_by(username = data_field.data).first():
+        if User.query.filter_by(user_name = data_field.data).first():
             raise ValidationError('The username is already taken')
 
 
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,34 +1,41 @@
-from flask import render_template, render_template,request, url_for
+from flask import render_template,flash,redirect, render_template,request, url_for
 from . import auth
 from .forms import SignUpform, SignInForm
 from flask_login import login_user
-
+from ..models import User
+from .. import db
 
 @auth.route('/signup', methods= ["GET","POST"])
 def signup():
 
-    form = SignUpform
+    form = SignUpform()
 
     if form.validate_on_submit():
 
-        user = User(user_email =form.email.data, user_username = form.username.data, user_password = form.password.data)
+        user = User(user_email =form.email.data, user_name = form.username.data, user_password = form.password.data)
         db.session.add(user)
         db.session.commit()
 
-        return redirect(url_for('.signin'))
+        return redirect(url_for('.login'))
 
         title = 'New Account'
 
-    return render_template('signup.html')
+    return render_template('auth/signup.html', form = form)
 
 
-@auth.route('/signin')
-def signin():
-    form = SignInForm
+@auth.route('/signin', methods = ["GET","POST"])
+def login():
+    form = SignInForm()
 
     if form.validate_on_submit():
 
-        user = User.query.filter_by(email = form.email.data).first()
+        user = User.query.filter_by(user_email = form.email.data).first()
 
         if user is not None:
-            login_user(user, login_form,remember.data)
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('index.html'))
+
+        flash ('invalid username or password')
+
+    title = 'Loging in'
+    return render_template('auth/login.html', form = form, title  = title)
